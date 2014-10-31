4f27558a855b56d4b18f69a11a09827c224cc527
kozo yamagata <tune002@gmail.com>
2014-10-31 19:24:05 +0900

    Require all classes

--- lib/view_data.rb
+++ lib/view_data.rb
@@ -1,4 +1,8 @@
+require 'view_data/data'
 require 'view_data/engine'
+require 'view_data/injecter'
+require 'view_data/null_data'
+require 'view_data/render_options'
 require 'view_data/rails/railtie'
 
 module ViewData
