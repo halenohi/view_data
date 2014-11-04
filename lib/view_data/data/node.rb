eb5d6c1b0989d49516fd5b416cf698aa45d81bb6
kozo yamagata <tune002@gmail.com>
2014-11-04 19:07:47 +0900

    リテラルに注入するmoduleをDelegateに分割して注入作業をメソッド化

--- lib/view_data/data/node.rb
+++ lib/view_data/data/node.rb
@@ -1,15 +1,29 @@
 module ViewData::Data::Node
   attr_accessor :__view_data_struct
 
-  def method_missing(method, *args, &block)
-    node = __view_data_find_node(method, args)
-    node.presence || super
+  class << self
+    def create_node(val, name, args)
+      val.extend(Delegate)
+      val.__view_data_struct = {
+        name: name,
+        args: args,
+        nodes: []
+      }
+      val
+    end
   end
 
-  def __view_data_find_node(method, args)
-    @__view_data_struct[:nodes].select{ |node|
-      struct = node.__view_data_struct
-      struct[:name] == method && struct[:args] == args
-    }.first
+  module Delegate
+    def method_missing(method, *args, &block)
+      node = __view_data_find_node(method, args)
+      node.presence || super
+    end
+
+    def __view_data_find_node(method, args)
+      @__view_data_struct[:nodes].select{ |node|
+        struct = node.__view_data_struct
+        struct[:name] == method && struct[:args] == args
+      }.first
+    end
   end
 end
