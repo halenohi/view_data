a10df18576f33ac03a6a90e18dfb9f0f9152f6a1
kozo yamagata <tune002@gmail.com>
2014-11-05 17:23:35 +0900

    ViewData.defineのspecを書いた

--- spec/lib/view_data_spec.rb
+++ spec/lib/view_data_spec.rb
@@ -21,22 +21,43 @@ describe ViewData do
     { template: 'template value' }
   end
 
-  before do
-    allow(render_options).to receive(:layout).and_return(:layout)
-    allow(render_options).to receive(:template).and_return(:template)
-
-    allow(data_class).to receive(:find).with(:layout).and_return(layout_data)
-    allow(data_class).to receive(:find).with(:template).and_return(template_data)
-    stub_const('ViewData::Data', data_class)
-  end
-
   describe '.inject' do
+    before do
+      allow(render_options).to receive(:layout).and_return(:layout)
+      allow(render_options).to receive(:template).and_return(:template)
+
+      allow(data_class).to receive(:find).with(:layout).and_return(layout_data)
+      allow(data_class).to receive(:find).with(:template).and_return(template_data)
+      stub_const('ViewData::Data', data_class)
+    end
+
     subject do
       injector.inject(render_options, {})
     end
 
     it 'find data and merge to view_data' do
       expect(subject).to eq(layout: 'layout value', template: 'template value')
+    end
+  end
+
+  describe '.define' do
+    before do
+      allow_any_instance_of(ViewData::DSL).to receive(:instance_eval)
+      allow_any_instance_of(ViewData::DSL).to receive(:data_nodes).and_return(
+        test_key: 'test val'
+      )
+
+      ViewData.define do
+        'test block'
+      end
+    end
+
+    subject do
+      ViewData.data_nodes
+    end
+
+    it 'run DSL and extract data_nodes' do
+      expect(subject).to eq(test_key: 'test val')
     end
   end
 end
